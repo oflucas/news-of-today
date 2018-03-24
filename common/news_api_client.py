b73c61a03661f38a7b31822564dbd11a68d5e9d5
Lucas LI <scofieldgood@gmail.com>
2018-03-24 15:51:57 +0800

    add news api client and test module of it

---
+++ common/news_api_client.py
@@ -0,0 +1,35 @@
+import requests
+from json import loads
+
+NEWS_API_ENDPOINT = 'https://newsapi.org/v1/'
+NEWS_API_KEY = 'f38e57ce023b4d44ac2945bea1b1e542'
+ARTICLES_API = 'articles'
+SORT_BY_TOP = 'top'
+
+CNN = 'cnn'
+DEFAULT_SOURCES = [CNN]
+
+def buildUrl(end_point=NEWS_API_ENDPOINT, api_name=ARTICLES_API):
+    return end_point + api_name
+
+def getNewsFromSource(sources=DEFAULT_SOURCES, sortBy=SORT_BY_TOP):
+    articles = []
+    for source in sources:
+        payload = {
+            'apiKey': NEWS_API_KEY,
+            'source': source,
+            'sortBy': sortBy
+        }
+        response = requests.get(buildUrl(), params=payload)
+        res_json = loads(response.content)
+
+        # check res and extract info
+        if (res_json is not None and
+            res_json['status'] == 'ok' and
+            res_json['source'] is not None):
+            # put source info into each articles
+            for news in res_json['articles']:
+                news['source'] = res_json['source']
+
+            articles.extend(res_json['articles'])
+    return articles
